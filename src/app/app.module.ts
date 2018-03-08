f3600c991955a803f8100e7ae6bbafbd4236b808
Larry Lane <larry.lane@vml.com>
2018-03-08 08:46:31 -0600

    initial commit of new cli generate structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {  TooltipDirective } from './directives/tooltip.directive';
+import { TooltipComponent, TooltipContainerDirective } from './directives/tooltip.component';
+import { CustomTooltipComponent } from './directives/tooltip-custom.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TooltipDirective,
+    TooltipComponent,
+    TooltipContainerDirective,
+    CustomTooltipComponent
   ],
   imports: [
     BrowserModule
   ],
+  entryComponents: [ TooltipComponent, CustomTooltipComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/directives/tooltip.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TooltipDirective } from './tooltip.directive';
+
+describe('TippyTooltipDirective', () => {
+  it('should create an instance', () => {
+    // const directive = new TooltipDirective();
+    // expect(directive).toBeTruthy();
+  });
+});
